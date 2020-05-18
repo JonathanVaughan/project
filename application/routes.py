ab436ceeeafa14e8d8e1245366b897012c1b9622
JonathanVaughan <jonathan.vaughan@live.com>
2020-05-18 13:34:48 +0000

    project

--- application/routes.py
+++ application/routes.py
@@ -31,3 +31,17 @@ def order():
 		db.session.commit()
 		return redirect(url_for('home'))
 	return render_template('order.html', title='Order', form=form)	
+
+@app.route('/stock', methods=['GET', 'POST'])
+def stock():
+	form - StockForm()
+	if form.validate_on_submit():
+		stockData = Stock(
+			pizza=form.pizza.data,
+			quantity=form.quantity.data
+		)
+		db.session.add(stockData)
+		db.session.commit()
+		return redirect(url_for('home'))
+	return render_template('stock.html', title='stock', form=form)
+	
