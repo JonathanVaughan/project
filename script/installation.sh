7758bae7473f150063935116e4378094f269c9d9
JonathanVaughan <jonathan.vaughan@live.com>
2020-05-22 10:23:09 +0000

    testing gunincorn

--- script/installation.sh
+++ script/installation.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 sudo apt update -y
-
+ 
 sudo apt install python3 -y
-
+ 
 sudo apt install python3-pip -y
-
+ 
 sudo apt install python3-venv -y
-
+ 
 python3 -m venv venv
-
-source ~/.bashrc
-
+ 
+cd /var/lib/jenkins/workspace/example_freestyle
+ 
 source /var/lib/jenkins/workspace/example_freestyle/venv/bin/activate
-
+ 
 pip3 install -r requirements.txt
-
+ 
+source ~/.bashrc
+ 
 gunicorn --bind=0.0.0.0:5000 app:app
 
